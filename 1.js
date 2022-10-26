8b23d9b4bf053b5ba38a85761979c4907e13c560
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-27 02:49:11 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -10,7 +10,7 @@ Ff
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body tuanyougou.js
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/2816143418/qx/main/1.js
 [mitm] 
 hostname = *.tuanyougou.*
 *******************************

cafe0b80dca9de63c6b6ae4efd29511c9c2f02e4
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-27 02:52:13 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,28 +1,8 @@
-/*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-Ff
+# TikTok
 
-脚本名称:Ff
+hostname = *.tiktokv.com, *.byteoversea.com, *.tik-tokapi.com
 
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
-群1077223830
-*******************************
-[rewrite_local]
-^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/2816143418/qx/main/1.js
-[mitm] 
-hostname = *.tuanyougou.*
-*******************************
-Surge
-
-[Script]
-^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ requires-body=1,max-size=0,script-path=tuanyougou.js
-
-[MITM]
-hostname = *.tuanyougou.*
-
-*******************************/
-var obj = JSON.parse($response.body);
-    obj.data.userId= 9999;
-    $done({body: JSON.stringify(obj)});
+(?<=_region=)CN(?=&) url 307 JP
+(?<=&mcc_mnc=)4 url 307 2
+^(https?:\/\/(tnc|dm)[\w-]+\.\w+\.com\/.+)(\?)(.+) url 302  $1$3
+(?<=\d\/\?\w{7}_\w{4}=)1[6-9]..(?=.?.?&) url 307 17

9fb23a0916574edaecb7c983c1198c486ae3dc28
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-27 10:47:36 +0800

    Update qx.js

--- qx.js
+++ qx.js
@@ -1,5 +1,16 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+Ff
+
+脚本名称:Ff
+
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
 [rewrite_local]
-^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://github.com/2816143418/qx/blob/main/qx.js
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body tuanyougou.js
 [mitm] 
 hostname = *.tuanyougou.*
 *******************************

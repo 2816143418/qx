a8f97c8808480462c69cc28cf58e0874e394f9a2
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-28 11:01:54 +0800

    Update Spotify Pro.js

--- Spotify Pro.js
+++ Spotify Pro.js
@@ -2,4 +2,4 @@ Spotify Pro解锁部分功能，音质不能设置超高  app版本8.7.78
 下载地址：http://mtw.so/6qFvqC
 #hostname= spclient.wg.spotify.com
 ****************************************/
-^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js
+^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/2816143418/qx/main/Spotify.js

f67d46ce2473c89c9e184aa7c037a6e79014f1ad
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-29 20:57:17 +0800

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,2 +1,24 @@
+/*******************************
+a
+脚本名称:a
 
-https://raw.githubusercontent.com/2816143418/qx/main/11.js
+*******************************
+[rewrite_local]
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/2816143418/qx/main/11.js
+[mitm] 
+hostname = *.tuanyougou.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ requires-body=1,max-size=0,script-path=tuanyougou.js
+
+[MITM]
+hostname = *.tuanyougou.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data.userId= 99999;
+    $done({body: JSON.stringify(obj)});
+
+

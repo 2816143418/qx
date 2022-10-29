199df1c90e88168cd8c2544f2f2d12883f245e5b
2816143418 <116750908+2816143418@users.noreply.github.com>
2022-10-29 23:19:31 +0800

    Update Spotify Pro.js

--- Spotify Pro.js
+++ Spotify Pro.js
@@ -2,4 +2,4 @@ Spotify Pro APP版本8.7.78
 下载地址：http://mtw.so/6qFvqC
 hostname= spclient.wg.spotify.com
 ****************************************/
-^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/2816143418/qx/main/Spotify.js
+^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/2816143418/ziyon/main/Spotify.js
